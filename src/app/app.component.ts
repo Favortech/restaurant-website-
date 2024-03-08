ea4e0d0ca5a407529f553019ff168c29b7d54d0c
Favortech <davidabolarinwa4@gmail.com>
2024-03-08 17:10:48 +0100

    resturant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+
+  constructor(private router: Router) {
+
+  }
+  enter(): void{
+    this.router.navigate(['dashboard'])
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'register', component: AppComponent},
+    {path:'dashboard', component: DashboardComponent}
+];
