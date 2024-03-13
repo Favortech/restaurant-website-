4052e50ac9afbc055257dabfa5da93142871b3b3
Favortech <davidabolarinwa4@gmail.com>
2024-03-13 16:06:43 +0100

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { IntroComponent } from './intro/intro.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    {path:'', component: HomepageComponent},
+    {path:'', component: IntroComponent},
+    {path:'intro', component: IntroComponent},
+    {path:'login', component: LoginComponent},
+    {path:"home", component:HomepageComponent},
     {path:'dashboard', component:DashboardComponent},
-    {path:'login', component: LoginComponent}
+    {path:'register', component:RegisterComponent},
 ];
